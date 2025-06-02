293e0db66e3aaa85d2a5ca017c5dd88d571ecc72
casper beckman <casbeck@outlook.com>
2025-06-02 15:43:20 +0200

    fixed to normal

--- JacIc.m
+++
@@ -1,9 +0,0 @@
-% Jacobi matrix for Ic
-function J = JacIc(atilde, x0tilde, y0tilde, alphas, eta, omega)
-     J = zeros(length(alphas),3);
-     for i = 1:length(alphas)
-         J(i,1) = eta * cos(omega * (x0tilde .* cos(alphas(i)) + y0tilde .* sin(alphas(i)))); %dF/da
-         J(i,2) = -atilde * eta .* sin(omega *(x0tilde .*cos(alphas(i)) + y0tilde .*sin(alphas(i))))*omega .*cos(alphas(i)); %dF/dx0
-         J(i,3) = -atilde * eta .* sin(omega *(x0tilde .*cos(alphas(i)) + y0tilde .*sin(alphas(i))))*omega .*sin(alphas(i)); %dF/dy0
-     end
-end
--- uppg4_2.m
+++
@@ -1,5 +0,0 @@
-%% Grafer av relativt dålig placering
-xs = 0.75;
-
-S = @(x, y) 1 * S0(x-xs, y-ys);
-[B, Sol] = hhsolver(omega, S, 1000);
